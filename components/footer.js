45cc71d94977917af2544ec41519ef2141346490
luchofazio27 <luchofazio@gmail.com>
2023-07-18 21:02:52 -0300

    creando footer y dandole css

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,30 @@
+import Link from "next/link"
+import styles from '../styles/footer.module.css'
 
 export default function Footer() {
   return (
-    <div>
-      footer
-    </div>
+    <footer className={styles.footer}>
+      <div className={`container ${styles.content}`}>
+      <nav className={styles.navigation}>
+            <Link href='/'>
+                Inicio
+            </Link>
+
+            <Link href='/we'>
+                Nosotros
+            </Link>
+
+            <Link href='/blog'>
+                Blog
+            </Link>
+
+            <Link href='/store'>
+                Tienda
+            </Link>
+        </nav>
+
+        <p className={styles.copyright}>Todos los derechos reservados { new Date().getFullYear() }</p>
+      </div>
+    </footer>
   )
 }
--- components/header.js
+++ components/header.js
@@ -1,27 +1,33 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 export default function Header() {
+
+  const router = useRouter()
   return (
     <header className={styles.header}>
       <div className={`container ${styles.bar}`}>
+        <Link href={'/'}>
         <Image src='/img/logo.svg' width={300} height={40} alt='Imagen logotipo'/>
+        </Link>
+        
 
         <nav className={styles.navigation}>
-            <Link href='/'>
+            <Link href='/' className={router.pathname === '/' ? styles.active : ''}>
                 Inicio
             </Link>
 
-            <Link href='/we'>
+            <Link href='/we' className={router.pathname === '/we' ? styles.active : ''}>
                 Nosotros
             </Link>
 
-            <Link href='/blog'>
+            <Link href='/blog' className={router.pathname === '/blog' ? styles.active : ''}>
                 Blog
             </Link>
 
-            <Link href='/store'>
+            <Link href='/store' className={router.pathname === '/store' ? styles.active : ''}>
                 Tienda
             </Link>
         </nav>
